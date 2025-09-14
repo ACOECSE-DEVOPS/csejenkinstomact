dc2dc2f1e3cb5721cd97d6dae16a5c1315fd27c3
ACOE DEVOPS <acoecolledecse@gmail.com>
2025-09-14 23:29:39 +0530

    jjj

---
+++ CSEJENKINSTOMCAT/src/main/java/com/example/demo/MInecontroller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MInecontroller {
+
+	@GetMapping("/home")
+	public String disp() {
+		 return "I MA DURGA PRASAD";
+	}
+}
